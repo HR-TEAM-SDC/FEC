6cdfc8beea410246986c9faab7ea20f2b9b6cea6
Jacob Blaettler <jrblaettler@gmail.com>
2021-12-21 16:48:12 -0700

    download axios

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h2>FEC</h2>
+      <h2>Hi guys</h2>
       <h3>Date : {new Date().toDateString()}</h3>
     </div>
   );
