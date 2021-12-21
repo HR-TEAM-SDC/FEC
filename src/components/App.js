681bed0ef67aad97c109d86b0c61a3651d10f760
Jacob Blaettler <jrblaettler@gmail.com>
2021-12-21 15:24:07 -0700

    download react and dev dependencies and did html/index and webpack boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h2>Welcome to React App</h2>
+      <h2>FEC</h2>
       <h3>Date : {new Date().toDateString()}</h3>
     </div>
   );
