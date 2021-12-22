403bc694d7026985b9cec46cebef78073a290c0e
Jacob Blaettler <jrblaettler@gmail.com>
2021-12-22 12:53:57 -0700

    base repo complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../apis/atelier';
 
 const App = () => {
+  const [test, setTest] = useState(null);
+
+  useEffect(() => {
+    axios.get('products').then(res => setTest(res.data));
+  }, []);
+
   return (
     <div>
       <h2>Hi guys</h2>
