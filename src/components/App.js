6087c31087f33c412d7c58b99d084cc911b8d51e
Jacob Blaettler <jrblaettler@gmail.com>
2021-12-24 16:05:10 -0700

    first merge test

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from '../apis/atelier';
-import ProductDetails from './ProductDetails/ProductDetails;
-import RelatedItems from './RelatedItems';
-
+import React, { useState, useEffect } from "react";
+import axios from "../apis/atelier";
+import ProductDetails from "./ProductDetails/ProductDetails";
+import RelatedItems from "./RelatedItems";
 
 const App = () => {
   const [test, setTest] = useState(null);
 
   useEffect(() => {
-    axios.get('products').then(res => setTest(res.data));
+    axios.get("products").then((res) => setTest(res.data));
   }, []);
 
   return (
-    <div>
-      <h2>Hi guys</h2>
-      <h3>Date : {new Date().toDateString()}</h3>
+    <main>
       <ProductDetails />
       <RelatedItems />
-    </div>
+    </main>
   );
 };
 
