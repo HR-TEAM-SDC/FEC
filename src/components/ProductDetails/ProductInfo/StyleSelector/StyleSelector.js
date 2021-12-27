b16a25a1066a5f3003c66a8f3cfbaad0d341cf25
Derek Wing <derekwingg@gmail.com>
2021-12-27 00:57:32 -0600

    Added styles

--- src/components/ProductDetails/ProductInfo/StyleSelector/StyleSelector.js
+++ src/components/ProductDetails/ProductInfo/StyleSelector/StyleSelector.js
@@ -15,7 +15,7 @@ const StyleSelector = () => {
 
   return (
     <h4>
-      Style Selector Component
+      Style Selector Component.
       <div>
         {styles.map((style) => {
           return <Style style={style} key={style.style_id} />;
