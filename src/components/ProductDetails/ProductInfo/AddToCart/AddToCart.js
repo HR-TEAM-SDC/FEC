e720241c27b473f818a9537204b5a07f2ba94917
Derek Wing <derekwingg@gmail.com>
2021-12-28 12:16:55 -0600

    Added quantity component

--- src/components/ProductDetails/ProductInfo/AddToCart/AddToCart.js
+++ src/components/ProductDetails/ProductInfo/AddToCart/AddToCart.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SizesSelector from "./SizesSelector";
+import Quantity from "./Quantity";
 
 const AddToCart = () => {
   return (
     <div>
       <h4>Add To Cart Component</h4>
       <SizesSelector />
+      <Quantity />
     </div>
   );
 };
--- src/components/ProductDetails/ProductInfo/AddToCart/Quantity.js
+++ src/components/ProductDetails/ProductInfo/AddToCart/Quantity.js
@@ -1,26 +1,37 @@
 import React, { useContext } from "react";
 import { StylesContext, CurrentStyleContext } from "../../ProductDetails";
 
-const SizesSelector = () => {
+const Quantity = () => {
   const styles = useContext(StylesContext);
   const currentStyle = useContext(CurrentStyleContext);
 
-  const renderSizes = () => {
+  const renderQuantity = () => {
+    let maxQuantity = 0;
     let content = [];
+    let quantity = 1;
+    console.log(
+      "currentStyle.currentStyle.skus:",
+      currentStyle.currentStyle.skus
+    );
     for (let key in currentStyle.currentStyle.skus) {
-      content.push(
-        <option> {currentStyle.currentStyle.skus[key].size} </option>
-      );
+      if (maxQuantity < currentStyle.currentStyle.skus[key].quantity) {
+        maxQuantity = currentStyle.currentStyle.skus[key].quantity;
+      }
+    }
+    console.log("maxQuantity:", maxQuantity);
+    for (let i = 0; i < maxQuantity; i++) {
+      content.push(<option>{quantity}</option>);
+      quantity++;
     }
     return content;
   };
 
   return (
     <div>
-      <h4>Sizes Component</h4>
-      Select Size: <select>{renderSizes()}</select>
+      <h4>Quantity Component</h4>
+      Quantity: <select>{renderQuantity()}</select>
     </div>
   );
 };
 
-export default SizesSelector;
+export default Quantity;
