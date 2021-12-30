cf65ec4afb348395e7847aae0316774d92466e6f
Derek Wing <derekwingg@gmail.com>
2021-12-30 15:13:41 -0600

    changed styling

--- src/components/ProductDetails/ImageGallery/MainImage.js
+++ src/components/ProductDetails/ImageGallery/MainImage.js
@@ -12,6 +12,7 @@ const MainImage = (props) => {
   // useEffect(() => {
   //   setCurrentImage(<img src="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg" style="max-width: 50px; max-height: 50px;"></img>);
   // }, []);
+  //a
 
   const mainImageStyle = {
     display: "block",
